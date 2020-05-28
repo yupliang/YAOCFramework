346850823f986a409bd80b5d414419e8e634b592
yupeiliang <yupeiliang@fotoable.com>
2020-05-28 18:24:40 +0800

    init

---
+++ YAOCFramework/YAUncaughtExceptionHandler.h
@@ -0,0 +1,18 @@
+//
+//  YAUncaughtExceptionHandler.h
+//  YAOCFramework
+//
+//  Created by app-01 on 2020/5/28.
+//  Copyright © 2020 app-01 org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YAUncaughtExceptionHandler : NSObject
++ (void)setDefaultHandler;
++ (NSUncaughtExceptionHandler*)getHandler;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YAOCFramework/YAUncaughtExceptionHandler.m
@@ -0,0 +1,44 @@
+//
+//  YAUncaughtExceptionHandler.m
+//  YAOCFramework
+//
+//  Created by app-01 on 2020/5/28.
+//  Copyright © 2020 app-01 org. All rights reserved.
+//
+
+#import "YAUncaughtExceptionHandler.h"
+
+NSString *applicationDocumentsDirectory() {
+    return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+}
+   
+void UncaughtExceptionHandler(NSException *exception) {
+     NSArray *arr = [exception callStackSymbols];
+     NSString *reason = [exception reason];
+     NSString *name = [exception name];
+   
+     NSString *url = [NSString stringWithFormat:@"=============异常崩溃报告=============\nname:\n%@\nreason:\n%@\ncallStackSymbols:\n%@",
+                   name,reason,[arr componentsJoinedByString:@"\n"]];
+     NSString *path = [applicationDocumentsDirectory() stringByAppendingPathComponent:@"Exception.txt"];
+     [url writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:nil];
+}
+
+@implementation YAUncaughtExceptionHandler
+
+#ifdef DEBUG
++ (void)load {
+    [YAUncaughtExceptionHandler setDefaultHandler];
+}
+#endif
+   
++ (void)setDefaultHandler
+{
+     NSSetUncaughtExceptionHandler (&UncaughtExceptionHandler);
+}
+   
++ (NSUncaughtExceptionHandler*)getHandler
+{
+     return NSGetUncaughtExceptionHandler();
+}
+
+@end
