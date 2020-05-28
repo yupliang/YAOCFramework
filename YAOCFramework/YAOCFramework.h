9b328594c9ea5cead3fe1d42a1f2a4c1382dd698
yupeiliang <yupeiliang@fotoable.com>
2020-05-28 18:14:54 +0800

    Initial Commit

---
+++ YAOCFramework/YAOCFramework.h
@@ -0,0 +1,19 @@
+//
+//  YAOCFramework.h
+//  YAOCFramework
+//
+//  Created by app-01 on 2020/5/28.
+//  Copyright © 2020 app-01 org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YAOCFramework.
+FOUNDATION_EXPORT double YAOCFrameworkVersionNumber;
+
+//! Project version string for YAOCFramework.
+FOUNDATION_EXPORT const unsigned char YAOCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YAOCFramework/PublicHeader.h>
+
+
